82ede408afceadcc32b46dd6b67b7ba51df195e2
bxbdev <bxbdev2022@gmail.com>
2023-04-12 01:06:11 +0800

    Made react project

---
+++ react-rtk-demo/src/features/user/userSlice.js
@@ -0,0 +1,56 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+const initialState = {
+    loading: false,
+    users: [],
+    error: ''
+}
+// Generates pending, fulfilled, rejected action types
+export const fetchUsers = createAsyncThunk('user/fetchUsers' , () => {
+    return axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then( response => response.data ) 
+})
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    extraReducers: (builder) => {
+        builder.addCase(fetchUsers.pending, (state) => {
+            state.loading = true
+        })
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.loading = false
+            state.users = action.payload
+            state.error = ''
+        })
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            /* action
+                {
+                    type: 'user/fetchUsers/rejected',
+                    meta: {
+                        arg: ...,
+                        requestId: ....,
+                        rejectedWithValue: false,
+                        requestStatus: 'rejected',
+                        aborted: false,
+                        condition: false
+                    },
+                    error: {
+                        name: 'AxiosError',
+                        message: 'Request failed with status code 404',
+                        stack: ....,
+                        code: 'ERR_BAD_REQUEST'
+                    }
+                }
+            */
+            // console.log(action)
+            state.loading = false
+            state.users = []
+            state.error = action.error.message
+        })
+    }
+})
+
+export default userSlice.reducer
